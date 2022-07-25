3711ccead20cd01e0550448f6e7b6323cc01af3c
ChakriSri-27 <91770676+ChakriSri-27@users.noreply.github.com>
2022-07-25 23:29:01 +0530

    Update Main.py

--- Main.py
+++ Main.py
@@ -1,38 +1,41 @@
 import os
 class Stack:
-    """This class contains the stack and its size
-    Attributes:
-    items: it is the stack
-    size: it holds the size of the stack"""
+    """ This class contains the stack and its size
+    Attributes: 
+        items: It is the stack
+        size: It holds the size of the stack"""
     
-    def __init__(self, size):
+    def _init_(self, size):
+        
         self.items = []
         self.size = size
 
     def is_empty(self):
         """The is_empty() function returns if the stack is empty"""
-        return len(self.items)==0
-            
+        return len(self.items)== 0
+
     def is_full(self):
         """The is_full() function returns if the stack is full"""
         return len(self.items)==self.size
-            
+
     def push(self, data):
-        """The push function pushes or adds the data to the stack
+        """ The push function pushes or adds the data to the stack
         Arguments:
-        data: it holds the value to be added to the stack"""
+        data: it holds the value to be added to the stack """
+        
         if not self.is_full():
             self.items.append(data)
 
     def pop(self):
-        """The pop function pops or delets the top most data from the stack"""
+        """the pop function pops or deletes the top most data from the stack"""
         if not self.is_empty():
-           self.items.pop(-1)
-
+            self.items.pop(-1)
+          
     def status(self):
-    """The status function displays the elements of the stack"""
-       for element in self.items:
-        print(element)
+        """The status function displays the element of the stack """
+        for element in self.items:
+            print(element)
+           
 
 # Do not change the following code
 size, queries = map(int, input().rstrip().split())
