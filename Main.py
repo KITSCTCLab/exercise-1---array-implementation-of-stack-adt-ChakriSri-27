a1b45fa3345641d08200b82d704d9862490f1710
ChakriSri-27 <91770676+ChakriSri-27@users.noreply.github.com>
2022-07-25 15:54:41 +0530

    Update Main.py

--- Main.py
+++ Main.py
@@ -5,24 +5,18 @@ class Stack:
         self.size = size
 
     def is_empty(self):
-        if len(self.items)==0:
-            return True
-        else:
-            return  False
-
+        return len(self.items)==0:
+            
     def is_full(self):
-        if len(self.items)==self.size:
-            return true
-        else:
-            return false
-
+        return len(self.items)==self.size:
+            
     def push(self, data):
         if not self.is_full():
             self.items.append(data)
 
     def pop(self):
         if not self.is_empty():
-           self.items.pop()
+           self.items.pop(-1)
 
     def status(self):
        for element in self.items:
